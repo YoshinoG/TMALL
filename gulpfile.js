2b0887b97415fc5e5c1174e18d957b6d08029dfc
gzj <745476203@qq.com>
2018-05-25 08:53:57 +0800

    create obj

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var connect = require("gulp-connect");
+var concat = require("gulp-concat");
+var ugligy = require("gulp-uglify");
+var rename = require("gulp-rename");
+var clean = require("gulp-clean-css");
+
+gulp.task("copy",function () {
+    gulp.src("*.html").pipe(gulp.dest("dist")).pipe(connect.reload());
+    gulp.src("js/*.js").pipe(ugligy()).pipe(gulp.dest("dist/js"));
+    gulp.src("sass/*.scss").pipe(sass()).pipe(clean()).pipe(gulp.dest("dist/css"));
+    gulp.src("images/*.{jpg,png,gif}").pipe(gulp.dest("dist/images"));
+})
+gulp.task('server',function(){
+    connect.server({
+        root:"dist",
+        livereload:true
+    });
+    gulp.watch(["*.html","js/*.js","sass/*.scss","images/*.{jpg,png,gif}"],["copy"]);
+})
+gulp.task("default",["copy","server"]);
